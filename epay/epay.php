08962d922888eb102c66119872d1b45803a48477
Søren Larsen <soren.larsen@bambora.com>
2016-11-29 09:12:14 +0100

    Bump version

--- epay/epay.php
+++ epay/epay.php
@@ -17,7 +17,7 @@ class EPay extends PaymentModule
 	public function __construct()
 	{
 		$this->name = 'epay';
-		$this->version = '4.9.1';
+		$this->version = '4.9.2';
 		$this->author = "ePay A/S (a Bambora Company)";
 		$this->tab = 'payments_gateways';
         $this->bootstrap = true;
