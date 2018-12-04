92d00a93d9695334a726e0ab7c3b26e7e5857330
MathiasReker <mathias@reker.dk>
2018-12-04 14:15:22 +0100

    remove extra forslash

--- epay/epay.php
+++ epay/epay.php
@@ -1107,7 +1107,7 @@ class EPay extends PaymentModule
                 $message = 'Autocapture failed with message: ' . $e->getMessage();
                 $this->createStatusChangesMessage($params['id_order'], $message);
                 $id_lang = (int) $this->context->language->id;
-                $dir_mail = _MODULE_DIR_ . '/mails/';
+                $dir_mail = _MODULE_DIR_ . 'mails/';
                 $mailTo = Configuration::get('EPAY_AUTOCAPTURE_FAILUREEMAIL');
                 Mail::Send($id_lang, 'autocapturefailed', 'Auto capture of ' . $params['id_order'] . ' failed', array('{message}' => $e->getMessage()), $mailTo, null, null, null, null, null, $dir_mail);
             }
